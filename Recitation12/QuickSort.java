3f98853dc6d7c63c54f9cdbbeb88a25371a28c78
adithyaround <adithya@roundfinance.com>
2024-05-03 18:29:50 -0400

    nicer quicksort

--- Recitation12/QuickSort.java
+++ Recitation12/QuickSort.java
@@ -11,21 +11,30 @@ public class QuickSort {
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+        int pivot = arr[low];  
+        int i = low;     
+        int j = high;
+        while (i < j) {
+            // Move 'j' towards 'low' until an element smaller than the pivot is found
+            while (i < j && arr[j] >= pivot) {
+                j--;
+            }
+            // Move 'i' towards 'high' until an element greater than the pivot is found
+            while (i < j && arr[i] <= pivot) {
                 i++;
+            }
+            if (i < j) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        return i + 1;
+        // Swap the pivot with the element at position 'j'
+        arr[low] = arr[j];
+        arr[j] = pivot;
+        return j;  // Return the position of pivot, which is now 'j'
     }
+
 
     public static void main(String[] args) {
         int[] array1 = {10, 7, 8, 9, 1, 5};
